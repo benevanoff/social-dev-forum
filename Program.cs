75e0294784ae81c8dd3f34605eeaf8c54bb43b6e
benevanoff <benjaminevanoff99@gmail.com>
2025-10-30 18:51:58 -0500

    Make APIs actually conform to design doc

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
- var database = new List<Dictionary<String, String>>();
+var database = new List<Dictionary<String, String>>();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Welcome to the developer's forum!");
 
 app.MapGet("/list", () => {
-    return database;
+    var response = new List<ListResponseItem>();
+    for (int i = 0; i < database.Count; i++) {
+        var item = new ListResponseItem();
+        item.id = i+1;
+        item.subject = database[i]["subject"];
+        response.Add(item);
+    }
+    return response;
 });
 
 app.MapPost("/create", async (HttpRequest request) => {
     var request_dict = await request.ReadFromJsonAsync<Dictionary<String, String>>();
     database.Add(request_dict);
+
+    var response_dict = new Dictionary<String, int>();
+    response_dict.Add("post_id", database.Count);
+    return response_dict;
+});
+
+app.MapGet("/post", (int id) => {
+    return database[id-1];
 });
 
 app.Run();
+
+public class ListResponseItem
+{
+    public int id { get; set; }
+    public String subject { get; set; }
+}
