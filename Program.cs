01ab8aa9346011f612ed0417dc0cfa441f63d990
Christopher Kim <229856188+haneulsea@users.noreply.github.com>
2025-11-08 19:42:48 -0800

    Add input validation for request at /create

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -17,12 +19,41 @@ app.MapGet("/list", () => {
 });
 
 app.MapPost("/create", async (HttpRequest request) => {
-    var request_dict = await request.ReadFromJsonAsync<Dictionary<String, String>>();
-    database.Add(request_dict);
-
-    var response_dict = new Dictionary<String, int>();
-    response_dict.Add("post_id", database.Count);
-    return response_dict;
+    IResult result;
+    try
+    {
+        var request_dict = await request.ReadFromJsonAsync<Dictionary<String, String>>();
+        if (request_dict.Count == 0 || request_dict.Count > 2)
+        {
+            return Results.BadRequest("Empty JSON or extra input. Request key/value pair count: " + request_dict.Count);
+        }
+        if (!request_dict.ContainsKey("subject") || string.IsNullOrWhiteSpace(request_dict["subject"]))
+        {
+            result = Results.BadRequest("Missing/empty subject");
+        }
+        else if (!request_dict.ContainsKey("body") || string.IsNullOrWhiteSpace(request_dict["body"]))
+        {
+            result = Results.BadRequest("Missing/empty body");
+        }
+        else
+        {
+            database.Add(request_dict);
+            var response_dict = new Dictionary<String, int>();
+            // adjust later when there is data persistence to respond with actual id
+            response_dict.Add("post_id", database.Count);
+            result = Results.Ok(response_dict);
+        }
+    }
+    catch (JsonException)
+    {
+        result = Results.BadRequest("Malformed JSON");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        result = Results.StatusCode(500);
+    }
+    return result;
 });
 
 app.MapGet("/post", (int id) => {
